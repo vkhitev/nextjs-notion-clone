a4f937e374d9b69e28a1fdff376154f3d2d83993
Vlad Khitev <vkhitev@gmail.com>
2023-10-14 01:33:44 +0300

    feat: add prettier config

---
+++ prettier.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  arrowParens: 'always',
+  bracketSpacing: true,
+  bracketSameLine: false,
+  jsxSingleQuote: false,
+  proseWrap: 'preserve',
+  printWidth: 100,
+  quoteProps: 'as-needed',
+  semi: false,
+  singleQuote: true,
+  tabWidth: 2,
+  trailingComma: 'all',
+  useTabs: false,
+}
